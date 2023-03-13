90f03f34337bd694206d4635863f2479b2e59d4b
Jableman19 <jableman@umich.edu>
2023-03-13 12:08:38 -0400

    HashRouter

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react"  
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route, HashRouter } from "react-router-dom"
 import Home from "./pages/home"
 import Nav from "./components/nav"
 import Projects from './pages/projects';
 function App() {
   return (
-    <Router >
+    <HashRouter>
       <Nav css={css({position: 'fixed'})}/>
       <Routes>
         <Route path ="/" element={<Home/>}/>
         <Route path ="/projects" element={<Projects/>}/>
       </Routes>
-    </Router>
+    </HashRouter>
   );
 }
 
